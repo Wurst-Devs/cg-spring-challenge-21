46a4f25aa24f2b0bd0a228a8f3aeacbaae2d289f
Klemek <Klemek@outlook.fr>
2021-05-06 20:40:00 +0200

    Update klemek.py

--- klemek.py
+++ klemek.py
@@ -2,7 +2,7 @@ import sys
 from types import GeneratorType
 from functools import reduce
 
-MAX_DAY = 23 # WOOD 2 => 0 / WOOD 1 => 5
+MAX_DAY = 23 # WOOD 2 => 0 / WOOD 1 => 5 / ONWARD => 23
 ALLOWED = [
     "GROW",  # WOOD 1
     "SEED",  # BRONZE
@@ -10,21 +10,15 @@ ALLOWED = [
 
 def debug(*values, name = None, end = '\n'):
     values = [tuple(v) if isinstance(v, GeneratorType) else v for v in values]
-    if name is not None:
-        print(f"{name} =", *values, file=sys.stderr, end=end, flush=True)
-    else:
-        print(*values, file=sys.stderr, end=end, flush=True)
-    if len(values) == 1:
-        return values[0]
+    if name is not None: print(f"{name} =", *values, file=sys.stderr, end=end, flush=True)
+    else: print(*values, file=sys.stderr, end=end, flush=True)
+    if len(values) == 1: return values[0]
     return tuple(values)
 
 # CLASSES
 
 class Cell:
     def __init__(self, *args):
-        # index: 0 is the center cell, the next cells spiral outwards
-        # richness: 0 if the cell is unusable, 1-3 for usable cells
-        # neigh_0: the index of the neighbouring cell for each direction
         self.id = int(args[0])
         self.richness = int(args[1])
         self.neighbors_raw = map(int, args[2:])
@@ -61,15 +55,14 @@ class Tree:
 
     def __init__(self, cells, sun_dir, *args):
         self.id = int(args[0])
-        self.cell = cells[self.id] # location of this tree
-        self.size = int(args[1]) # size of this tree: 0-3
-        self.is_mine = args[2] == "1" # 1 if this is your tree
-        self.is_dormant = args[3] == "1" # 1 if this tree is dormant
+        self.cell = cells[self.id]
+        self.size = int(args[1])
+        self.is_mine = args[2] == "1"
+        self.is_dormant = args[3] == "1"
         if self.is_mine:
             Tree.count[self.size] += 1
         self.cell.update(sun_dir, self)
         
-    
     def __repr__(self):
         if self.size == 3:
             return f"T{self.cell}=>{self.size}/{self.score}"
@@ -93,7 +86,6 @@ class Tree:
         return self.history.count(self.size)
 
 
-
 # INIT
 
 cells = [Cell(*input().split()) for _ in range(int(input()))]
@@ -106,34 +98,25 @@ day = -1
 # GAME LOOP
 while True:
     last_day = day
-    day = int(input())  # the game lasts 24 days: 0-23
+    day = int(input())
     turn_start = day != last_day
 
     sun_dir = day % 6
 
     Tree.reset_count()
-    Tree.nutrients = int(input())  # the base score you gain from the next COMPLETE action
+    Tree.nutrients = int(input())
 
     debug("nutrients:", Tree.nutrients)
 
-    # sun: your sun points
-    # score: your current score
     sun, score = [int(i) for i in input().split()]
-    inputs = input().split()
-    opp_sun = int(inputs[0])  # opponent's sun points
-    opp_score = int(inputs[1])  # opponent's score
-    opp_is_waiting = inputs[2] != "0"  # whether your opponent is asleep until the next day
+    opp_sun, opp_score, opp_is_waiting = map(int, input().split())
 
     for cell in cells:
         cell.reset()
 
     trees = [Tree(cells, sun_dir, *input().split()) for _ in range(int(input()))]
 
-    for cell in cells:
-        if cell.shadow > 0:
-            debug(cell)
-
-    [input() for _ in range(int(input()))]
+    [input() for _ in range(int(input()))]  # possible actions ignored
 
     mine = [tree for tree in trees if tree.is_mine]
     available = [tree for tree in mine if not tree.is_dormant]
